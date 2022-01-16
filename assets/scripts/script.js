efca583fc920fd38eb6cada6229ddb7a862a02d4
augustosplett <augusto.splett@gmail.com>
2022-01-16 00:24:36 +0000

    inserido arquivo js para controle do fechamento do menu sanduíche no menu responsivo

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+function fechar() {
+
+    var inputCheckbox = document.getElementById('menu-check');
+
+    if (inputCheckbox.checked === true) {
+        inputCheckbox.checked = false;
+    }
+
+}
